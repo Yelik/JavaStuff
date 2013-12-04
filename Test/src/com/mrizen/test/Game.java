3b967effdcc209fcf5e15200a8cf70587aa435d4
yelik newo <yeliknewo@gmail.com>
2013-12-03 20:48:27 -0500

    Video 4

--- Test/src/com/mrizen/test/Game.java
+++ Test/src/com/mrizen/test/Game.java
@@ -1,11 +1,13 @@
 package com.mrizen.test;
 
 import java.awt.Canvas;
+import java.awt.Dimension;
 
 import javax.swing.JFrame;
 
 public class Game extends Canvas implements Runnable {
-
+	private static final long serialVersionUID = 1L;
+	
 	public static int width = 300;
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
@@ -13,6 +15,13 @@ public class Game extends Canvas implements Runnable {
 	private Thread thread;
 	private JFrame frame;
 	private boolean running = false;
+
+	public Game() {
+		Dimension size = new Dimension(width * scale, height * scale);
+		setPreferredSize(size);
+		
+		frame = new JFrame();
+	}
 
 	public synchronized void start() {
 		running = true;
@@ -33,5 +42,18 @@ public class Game extends Canvas implements Runnable {
 		while (running) {
 			
 		}
+	}
+	
+	public static void main(String[] args){
+		Game game = new Game();
+		game.frame.setResizable(false);
+		game.frame.setTitle("Test");
+		game.frame.add(game);
+		game.frame.pack();
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.frame.setLocationRelativeTo(null);
+		game.frame.setVisible(true);
+		
+		game.start();
 	}
 }
