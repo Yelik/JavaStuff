61aabe0291ab199698a6e34ddac15b44f6b1ffec
yelik newo <yeliknewo@gmail.com>
2013-12-03 20:15:59 -0500

    Removed more files

--- Test/src/com/mrizen/test/Game.java
+++ Test/src/com/mrizen/test/Game.java
@@ -1,12 +1,17 @@
 package com.mrizen.test;
 
-public class Game implements Runnable {
+import java.awt.Canvas;
+
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
 
 	public static int width = 300;
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
 
 	private Thread thread;
+	private JFrame frame;
 	private boolean running = false;
 
 	public synchronized void start() {
